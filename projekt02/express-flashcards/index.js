8f332e2bb48e8bfc6cd4ca613e761d037b8b37b6
Verbosal <lukaszplfajnyy@gmail.com>
2025-10-16 08:51:23 +0000

    touched sum files

--- projekt02/express-flashcards/index.js
+++ projekt02/express-flashcards/index.js
@@ -1,14 +1,10 @@
 import express from "express";
 
 const port = 8000;
+const app = express();
 
-const app = express(); // Tworzymy obiekt aplikacji/serwera Express
+app.use(express.static("public")); // "magiczna" obsługa ścieżek dla plików z katalogu public
 
-app.get("/", (req, res) => { // Definiujemy handler dla metody GET ścieżki "/"
-  res.set("Content-Type", "text/plain"); // Zwracamy czysty tekst
-  res.send("Hello world"); // Wysyłamy tekst hello world
-});
-
-app.listen(port, () => { // Startujemy serwer na porcie 8000
+app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
