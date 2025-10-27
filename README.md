787129bc5e8f85a4726160a2eaae5b6c68285eb4
Verbosal <lukaszplfajnyy@gmail.com>
2025-10-27 08:36:05 +0000

    HI

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ node index.js
 On index.js in projekt02/express-flashcards/index.js runs a local server hosted at localhost:8000 (Same as 127.0.0.1:8000).<br><br>
 You can then explore to find /cards/categories.<br><br>
 The server handles inappriopriate path requests and returns a 404 page, along with illegal method combinations.<br><br><br>
-index.html and /cards/categories both can receive GET, but only /cards/categories can receive POST, so it's as follows:<br>
+index.html and /cards/categories can both handle GET, but only /cards/categories can handle POST, so it's as follows:<br>
 - index.html and GET<br>
 - /cards/categories and GET<br>
 - /cards/categories and POST<br>
--- projekt02/express-flashcards/index.js
+++ projekt02/express-flashcards/index.js
@@ -1,6 +1,9 @@
-import express from "express";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express"; // Express
+import {fileURLToPath} from 'url'; // Part 1 of getting the file path
+import {dirname} from 'path'; // Part 2 of getting the file path
+import bodyParser from 'body-parser'; // Part of handling POST requests
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false }); // Allows for handling POST requests
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,30 +19,31 @@ app.set("view engine", "ejs");
 // Use uhhh public, yeah
 app.use(express.static("public"));
 
-// Render /cards/categories/
-function renderCategories(req, res) {
+// GET method for /cards/categories/ and render
+app.get("/cards/categories/", (req, res) => {
   res.render("categories", {
     title: "Kategorie",
     categories: card_categories,
   });
-}
+});
 
-// GET method for /cards/categories/
-app.get("/cards/categories/", (req, res) => renderCategories(req, res));
+// POST method for /cards/categories/ and render with parameters
+app.post("/cards/categories/",urlencodedParser, (req, res) => {
+  res.render("categories", {
+    title: "Kategorie",
+    categories: card_categories,
+    parameters: req.body
+  });
 
-// POST method for /cards/categories/
-app.post("/cards/categories/", (req, res) => {
-  renderCategories(req, res);
+  // if (Object.keys(req.body).length > 0) {
+  //   console.log("h");
+  //   app.send("Podane parametry używając metody POST:");
 
-  console.log(req);
-
-  if (req.query.length > 0) {
-    app.send("Podane parametry używając metody POST:");
-
-    for (parameter in req.query) {
-      app.send(parameter);
-    }
-  }
+  //   for ({key, value} in req.body) {
+  //     (`${key} : ${value}`);
+  //     console.log(key, value);
+  //   }
+  // }
 });
 
 // Return 404 upon an improper path request
