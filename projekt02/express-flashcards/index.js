fee5c5f1224c30b59757ec860fb8df3191bd021f
Verbosal <lukaszplfajnyy@gmail.com>
2025-11-03 08:00:19 +0000

    Actually added POST supports YIPPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- projekt02/express-flashcards/index.js
+++ projekt02/express-flashcards/index.js
@@ -3,7 +3,7 @@ import {fileURLToPath} from 'url'; // Part 1 of getting the file path
 import {dirname} from 'path'; // Part 2 of getting the file path
 import bodyParser from 'body-parser'; // Part of handling POST requests
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false }); // Allows for handling POST requests
+const urlencodedParser = bodyParser.urlencoded({ extended: true }); // Allows for handling POST requests
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,35 +19,28 @@ app.set("view engine", "ejs");
 // Use uhhh public, yeah
 app.use(express.static("public"));
 
-// GET method for /cards/categories/ and render
-app.get("/cards/categories/", (req, res) => {
-  res.render("categories", {
-    title: "Kategorie",
-    categories: card_categories,
-  });
+function renderCategories(req, res, parameters) {
+  // Render categories
+  res.locals.title = "Kategorie";
+  res.locals.categories = card_categories;
+  res.locals.parameters = parameters;
+
+  res.render("categories");
+}
+
+// GET method for /cards/categories/ and then render
+app.get("/cards/categories", (req, res) => {
+  renderCategories(req, res);
 });
 
 // POST method for /cards/categories/ and render with parameters
 app.post("/cards/categories/",urlencodedParser, (req, res) => {
-  res.render("categories", {
-    title: "Kategorie",
-    categories: card_categories,
-    parameters: req.body
-  });
-
-  // if (Object.keys(req.body).length > 0) {
-  //   console.log("h");
-  //   app.send("Podane parametry używając metody POST:");
-
-  //   for ({key, value} in req.body) {
-  //     (`${key} : ${value}`);
-  //     console.log(key, value);
-  //   }
-  // }
+  // Add to the cards render optional POST parameters
+  renderCategories(req, res, req.body);
 });
 
 // Return 404 upon an improper path request
-app.use(function(req, res, next) {
+app.use(function(req, res) {
   res.status(404);
 
   if (req.accepts('html')) {
